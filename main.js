3fb299b93d972f225ab8553215498597109479cd
alvigr <44508235+alvigr@users.noreply.github.com>
2019-03-25 21:51:27 +0300

    Реализовал счетчик дохода

---
+++ main.js
@@ -0,0 +1,24 @@
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf('874168391:AAFNfF0eMO-zd-KwyorWvnYpogGERwZJ5RI')
+
+let income =0
+let expense =0
+
+const addIncome = (ctx) => {
+    console.log("Пользователь", ctx.message.from.username,)
+    console.log("Увеличить доход", ctx.message.text)
+    let transaction = Number(ctx.message.text.substring(1))
+    income += transaction
+    ctx.reply("Доход: " + income + " Расход: " + expense)
+}
+
+const addExpense = (ctx) => {
+    console.log("Увеличить расход", ctx.message.text)
+}
+
+bot.hears(/^\+/, addIncome)
+bot.hears(/^\-/, addExpense)
+
+console.log("Сервер бота запущен")
+bot.launch()
