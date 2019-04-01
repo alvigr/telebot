c2773c2e552bc3f2f4a8cc02c236e4667e455a31
alvigr <44508235+alvigr@users.noreply.github.com>
2019-04-01 23:45:09 +0300

    Избавился от дублирования кода

--- main.js
+++ main.js
@@ -4,20 +4,27 @@ const bot = new Telegraf('874168391:AAFNfF0eMO-zd-KwyorWvnYpogGERwZJ5RI')
 
 let store = {};
 
+const getUserStore = (ctx) => {
+  let username = ctx.message.from.username
+  if (!store[username]) {
+    store[username] = {
+      income: 0,
+      expense: 0
+    }
+  }
+  return store[username]
+}
+
+const replyBalance = (ctx, userStore) => {
+  ctx.reply("Доход: " + userStore.income + " Расход: " + userStore.expense)
+}
+
 const addIncome = (ctx, value) => {
   console.log("Увеличить доход", value)
-  let username = ctx.message.from.username
   if (value) {
-    if (!store[username]) {
-      store[username] = {
-        income: 0,
-        expense: 0
-      }
-    }
-
-    store[username].income += value
-
-    ctx.reply("Доход: " + store[username].income + " Расход: " + store[username].expense)
+    let userStore = getUserStore(ctx)
+    userStore.income += value
+    replyBalance(ctx, userStore)
   } else {
     ctx.reply("Вы ввели неправильное значение! Я понимаю только числа со знаком плюс или минус в начале!")
   }
@@ -25,18 +32,10 @@ const addIncome = (ctx, value) => {
 
 const addExpense = (ctx, value) => {
   console.log("Увеличить расход", value)
-  let username = ctx.message.from.username
   if (value) {
-    if (!store[username]) {
-      store[username] = {
-        income: 0,
-        expense: 0
-      }
-    }
-
-    store[username].expense += value
-
-    ctx.reply("Доход: " + store[username].income + " Расход: " + store[username].expense)
+    let userStore = getUserStore(ctx)
+    userStore.expense += value
+    replyBalance(ctx, userStore)
   } else {
     ctx.reply("Вы ввели неправильное значение! Я понимаю только числа со знаком плюс или минус в начале!")
   }
@@ -56,18 +55,18 @@ const addExpenseWithSign = (ctx) => {
 
 const addIncomeCommand = (ctx) => {
   console.log("Увеличить доход", ctx.message.text)
-  let value = Number(ctx.message.text.split(" ")[1])
+  let value = Number(ctx.message.text.split(" ", 2)[1])
   addIncome(ctx, value)
 }
 
 const addExpensCommand = (ctx) => {
   console.log("Увеличить расход", ctx.message.text)
-  let value = Number(ctx.message.text.split(" ")[1])
+  let value = Number(ctx.message.text.split(" ", 2)[1])
   addExpense(ctx, value)
 }
 
 bot.hears(/^\+/, addIncomeWithSign)
-bot.hears(/^\-/, addExpenseWithSign)
+bot.hears(/^-/, addExpenseWithSign)
 
 bot.command('income', addIncomeCommand)
 bot.command('expense', addExpensCommand)
