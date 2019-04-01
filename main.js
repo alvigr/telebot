3c87a5cae0c2a367872d66a6e8549543b8d39662
alvigr <44508235+alvigr@users.noreply.github.com>
2019-04-01 20:39:29 +0300

    Исправлено увеличение доходов и расходов

--- main.js
+++ main.js
@@ -5,24 +5,26 @@ const bot = new Telegraf('874168391:AAFNfF0eMO-zd-KwyorWvnYpogGERwZJ5RI')
 let store = {};
 
 const addIncomeOrExpense = (ctx) => {
-    console.log("Увеличить доход или расход", ctx.message.text)
-    let username = ctx.message.from.username
-    if (Number(ctx.message.text.substring(1))) {
-      if (!store[username]) {
-        store[username] = {
-          income: 0,
-          expense: 0
-        }
+  console.log("Увеличить доход или расход", ctx.message.text)
+  let username = ctx.message.from.username
+  let value = Number(ctx.message.text.substring(1))
+  console.log(value)
+  if (value) {
+    if (!store[username]) {
+      store[username] = {
+        income: 0,
+        expense: 0
       }
-      if (ctx.message.text.substring(0, 1) === '+') {
-        store[username].income = Number(ctx.message.text.substring(1))
-      } else {
-        store[username].expense = Number(ctx.message.text.substring(1))
-      }
-      ctx.reply("Доход: " + store[username].income + " Расход: " + store[username].expense)
-    } else {
-      ctx.reply("Вы ввели неправильное значение! Я понимаю только числа со знаком плюс или минус в начале!")
     }
+    if (ctx.message.text.substring(0, 1) === '+') {
+      store[username].income += value
+    } else {
+      store[username].expense += value
+    }
+    ctx.reply("Доход: " + store[username].income + " Расход: " + store[username].expense)
+  } else {
+    ctx.reply("Вы ввели неправильное значение! Я понимаю только числа со знаком плюс или минус в начале!")
+  }
 }
 
 const messageErrors = (ctx) => {
@@ -31,7 +33,9 @@ const messageErrors = (ctx) => {
 
 bot.hears(/[+-]/, addIncomeOrExpense)
 
-bot.hears(/[^+-0-9]/, messageErrors)
+//bot.hears(/[^+-0-9]/, messageErrors)
+
+bot.command('hipster', Telegraf.reply('λ'))
 
 console.log("Сервер бота запущен")
 
