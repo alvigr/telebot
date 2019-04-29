44ebb0fb3d9391ddfe5fd55533bd3eeb0eed57fc
alvigr <44508235+alvigr@users.noreply.github.com>
2019-04-29 21:13:53 +0300

    Добавили в API метод /balance

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const fastify = require('fastify')({
 fastify.register(require('fastify-cors'), {})
 
 const db = new sqlite3.Database('db.sqlite')
+const USERNAME = 'ivan'
 
 // Declare a route
 fastify.get('/', (request, reply) => {
@@ -24,10 +25,20 @@ fastify.post('/transactions', (request, reply) => {
   console.log(request.body)
   db.run(
     "INSERT INTO transactions (username, amount, comment) VALUES (?, ?, ?)",
-    'backend', request.body.amount, request.body.comment
+    USERNAME, request.body.amount, request.body.comment
   )
   reply.send({status: 'ok'})
 })
+
+fastify.get('/balance', (request, reply) => {
+    db.get("SELECT SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS income, " +
+      "SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS expense  " +
+      "FROM transactions WHERE username = ?", USERNAME, (err, row) => {
+      console.log(err)
+      reply.send(row)
+    })
+}
+)
 
 // Run the server!
 fastify.listen(3000, (err, address) => {
